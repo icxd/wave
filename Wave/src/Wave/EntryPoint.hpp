7b28439e93d6085a35e3a5725912543c10f2d280
icxd <icoding666@gmail.com>
2024-08-21 11:51:15 +0200

    use clang 18 and c++23

--- Wave/src/Wave/EntryPoint.hpp
+++ Wave/src/Wave/EntryPoint.hpp
@@ -7,13 +7,11 @@ extern wave::Application *wave::CreateApplication();
 
 #ifdef WAVE_PLATFORM_WINDOWS
 #error Windows is not supported yet
-#elifdef WAVE_PLATFORM_LINUX
+#else
 int main(int argc, char *argv[]) {
   wave::Log::Init();
   auto app = wave::CreateApplication();
   app->Run();
   delete app;
 }
-#else
-#error The platform you're on isn't supported yet.
 #endif
