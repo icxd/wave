40487e6435bb0ad4e8c1272e2658e29ec2b6bd6a
icxd <icoding666@gmail.com>
2024-08-22 18:03:00 +0200

    Add Timestep class

---
+++ Wave/src/Wave/Core/Timestep.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace wave {
+
+class Timestep {
+public:
+  Timestep(float time = 0.0f) : m_time(time) {}
+
+  operator float() const { return m_time; }
+
+  float GetSeconds() const { return m_time; }
+  float GetMilliseconds() const { return m_time * 1000.0f; }
+
+private:
+  float m_time;
+};
+
+} // namespace wave
